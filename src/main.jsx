2a263cea0aca038318d1d52460c434b077565853
Birkan <birkan.ydkrds@gmail.com>
2026-07-20 16:28:50 +0300

    strict mode kaldirildi cift mesaj duzeltildi

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AppProvider } from './lib/store'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AppProvider>
-      <App/>
-    </AppProvider>
-  </React.StrictMode>
+  <AppProvider>
+    <App/>
+  </AppProvider>
 )
